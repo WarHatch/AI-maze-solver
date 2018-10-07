5ad16e5b590befa71a161629e86ece833fd2846c
WarHatch <satelistas@gmail.com>
2018-10-07 13:05:41 +0300

    Classic backtracking instead of backtrack 1

--- Labirinth/Wanderer.cs
+++ Labirinth/Wanderer.cs
@@ -45,10 +45,10 @@ namespace Labirinth
             var falsePosition = moveStack.Pop().to;
 
             //  Classic backtracking
-            //board.PlaceOn(falsePosition, -1);
+            board.PlaceOn(falsePosition, -1);
 
             //  "Backtrack1"
-            board.PlaceBacktrackToken(falsePosition);
+            //board.PlaceBacktrackToken(falsePosition);
         }
 
         private string CreateLog(Point newPosition, int manueverIndex, string appendix)
